5ef03dcadc913ed6d001c1108da51f2c02f6123e
webdevelopermagic <152077263+webdevelopermagic@users.noreply.github.com>
2023-11-26 22:18:09 +0530

    test

---
+++ javafile.js
@@ -0,0 +1,19 @@
+let product-img = document.getElementById("product-img");
+let small-img = document.getElementById("small-img")
+
+    small-img[0].onclick = function()
+    {
+        product-img.src = small-img[0].src;
+    }
+    small-img[1].onclick = function()
+    {
+        product-img.src = small-img[1].src;
+    }
+    small-img[2].onclick = function()
+    {
+        product-img.src = small-img[2].src;
+    }
+    small-img[3].onclick = function()
+    {
+        product-img.src = small-img[3].src;
+    }
